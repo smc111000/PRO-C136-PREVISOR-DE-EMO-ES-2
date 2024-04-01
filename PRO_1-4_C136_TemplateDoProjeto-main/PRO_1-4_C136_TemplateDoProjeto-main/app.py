b258815d9891d8d3c4300e52aae23d1f6c200e05
Samuel GM <110042220+smc111000@users.noreply.github.com>
2024-04-01 19:59:32 -0300

    Update app.py

--- PRO_1-4_C136_TemplateDoProjeto-main/PRO_1-4_C136_TemplateDoProjeto-main/app.py
+++ PRO_1-4_C136_TemplateDoProjeto-main/PRO_1-4_C136_TemplateDoProjeto-main/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , request , jsonify
+from flask import Flask, render_template, request, jsonify
 import prediction
 
 app = Flask(__name__)
@@ -8,48 +8,35 @@ def home():
     return render_template('index.html')
 
 # api ouvindo solicitações POST e prevendo sentimentos
-@app.route('/predict' , methods = ['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-
     response = ""
     review = request.json.get('customer_review')
     if not review:
-        response = {'status' : 'error',
-                    'message' : 'Avaliação em Branco'}
-    
+        response = {'status': 'error',
+                    'message': 'Avaliação em Branco'}
     else:
-
-        # chamando o método predict do módulo de previsão.py
-        sentiment , path = prediction.predict(review)
-        response = {'status' : 'success',
-                    'message' : 'Got it',
-                    'sentiment' : sentiment,
-                    'path' : path}
-
+        sentiment, path = prediction.predict(review)
+        response = {'status': 'success',
+                    'message': 'Got it',
+                    'sentiment': sentiment,
+                    'path': path}
     return jsonify(response)
 
-
 # Criando uma API para salvar a avaliação. O usuário clica no botão Salvar
-@app.route('/' , methods = [''])
+@app.route('/save', methods=['POST'])
 def save():
+    date = request.json.get('date')
+    product = request.json.get('product')
+    review = request.json.get('review')
+    sentiment = request.json.get('sentiment')
+    data_entry = f"{date}, {product}, {review}, {sentiment}\n"
 
-    # extraindo data , nome do produto , avaliação e sentimento associado aos dados JSON
-    date = request.json.get('')
-    product = request.json.get('')
-    review = request.json.get('')
-    sentiment = request.json.get('')
+    with open('data_file.txt', 'a') as file:
+        file.write(data_entry)
 
-    # criando uma variável final separada por vírgulas
-    data_entry = date + "," + product + "," + review + "," + sentiment
+    return jsonify({'status': 'success', 
+                    'message': 'Dados Registrados'})
 
-    # abra o arquivo no modo 'append'
-
-    # Registre os dados no arquivo
-
-    # retorne uma mensagem de sucesso
-    return jsonify({'status' : 'success' , 
-                    'message' : 'Dados Registrados'})
-
-
-if __name__  ==  "__main__":
-    app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug=True)
